fe367e119b762bd7f6ac28fa1be9580c5da1ff49
MohamedGado <boogado@yahoo.com>
2022-04-15 20:08:02 +0200

    Full order page with date pickers

---
+++ dataBase.php
@@ -0,0 +1,108 @@
+<?php
+class DataBase
+{
+    private $serverName;
+    private $userName;
+    private $userPass;
+    private $dbName;
+    private $charSet;
+    private $dsn;
+    private $db;
+
+    public function __construct()
+    {
+        $this->serverName  = "localhost";
+        $this->userName = "root";
+        $this->userPass  = "Awad36148";
+        $this->dbName = "cafeteria";
+        $this->charSet = "utf8mb4";
+
+        $this->dsn = "mysql:host=" . $this->serverName . "; dbname=" . $this->dbName . "; charset=" . $this->charSet;
+    }
+    public function connect()
+    {
+        try {
+            $this->db = new PDO($this->dsn, $this->userName, $this->userPass);
+        } catch (PDOException $err) {
+            die($err->getMessage());
+        }
+    }
+    public function selectOrdersByDate($uid, $from, $to)
+    {
+
+        try {
+            $query = "SELECT * FROM  orders WHERE  orders.user_id =" . $uid . " AND date between '$from' and '$to'";
+
+
+            $stmt = $this->db->prepare($query);
+            $stmt->execute();
+            $user_orders = $stmt->fetchAll();
+            return $user_orders;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    public function selectAllByTableName($table_name)
+    {
+        $query = "SELECT * FROM $table_name";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }
+    public function selectUserOrders($uid)
+    {
+
+        try {
+            $query = 'SELECT * FROM orders where  orders.user_id = ' . $uid . ' ORDER BY date DESC';
+
+            $stmt = $this->db->prepare($query);
+            $stmt->execute();
+            $user_orders = $stmt->fetchAll();
+            return $user_orders;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function changeStatus($id)
+    {
+        try {
+
+            $sql = 'UPDATE `orders` SET `status`= "Canceled" WHERE id =' . $id . ' ';
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo $sql . "<br>" . $e->getMessage();
+            return false;
+        }
+    }
+    public function cancelOrder($id)
+    {
+        try {
+            $sql = "DELETE FROM orders WHERE id = $id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo $sql . "<br>" . $e->getMessage();
+            return false;
+        }
+    }
+    public function getProductsInOrders($oid)
+    {
+        try {
+            $sql_order = 'SELECT id, name,  price , picture , product_id, order_id, quantity  FROM orders_products
+             JOIN products 
+             ON orders_products.product_id = products.id
+              WHERE orders_products.order_id = ' . $oid;
+            $stat = $this->db->prepare($sql_order);
+            $stat->execute();
+            $orders_products = $stat->fetchAll();
+            return $orders_products;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+}
