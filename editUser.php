3b9df1ebf3fd1c917d7d9b0684f98c72cf0433e3
ahmedmohamed1990 <ahmed7mohamed1996@gmail.com>
2022-04-24 16:58:40 -0700

    add

--- editUser.php
+++ editUser.php
@@ -4,7 +4,7 @@ ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
 if (isset($_GET["errors"])){
   $errors = json_decode($_GET["errors"]);
-   var_dump($errors);
+   //var_dump($errors);
   
 }
 if (isset($_GET["olddata"])){
@@ -91,7 +91,7 @@ $row = $selectUser-> select_row($record_id);
   
   <div class="relative z-0 mb-6 w-full group">
       <input type="email" name="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"value="<?php echo $row->email?>" /> 
-      <label for="floating_email" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>";
+      <label for="floating_email" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
       <?php
                         if(isset($errors->email)){
                             echo "<p style='color: red'> $errors->email</p>";
@@ -100,7 +100,7 @@ $row = $selectUser-> select_row($record_id);
     </div>
   <div class="relative z-0 mb-6 w-full group">
       <input type="password" name="password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"value=""/>
-      <label for="floating_password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>";
+      <label for="floating_password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
      <?php 
                         if(isset($errors->password)){
                             echo "<p style='color: red'> $errors->password</p>";
@@ -109,7 +109,7 @@ $row = $selectUser-> select_row($record_id);
     </div>
   <div class="relative z-0 mb-6 w-full group">
       <input type="password" name="repeatpassword" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value=""/>
-      <label for="floating_repeat_password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>";
+      <label for="floating_repeat_password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
                      <?php  
                       if(isset($errors->repeatpassword)){
                             echo "<p style='color: red'> $errors->repeatpassword</p>";
@@ -119,7 +119,7 @@ $row = $selectUser-> select_row($record_id);
   <div class="grid xl:grid-cols-2 xl:gap-6">
     <div class="relative z-0 mb-6 w-full group">
         <input type="number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="roomNum" id="roomNum" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value="<?php echo $row->roomNum?>"   />
-        <label for="roomNum" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> Room No</label>";
+        <label for="roomNum" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> Room No</label>
         <?php           
         if(isset($errors->roomNum)){
                             echo "<p style='color: red'> $errors->roomNum</p>";
@@ -138,7 +138,7 @@ $row = $selectUser-> select_row($record_id);
   <div class="col-md-6">
                         <label for="validationCustom03" class="form-label">Image</label>
                         <input type="file" name="img" class=" form-label"   id="validationCustom03">
-                      }
+                      
                             <label style="color: red">
                             <?php
                             if (isset($_GET["emptyimg"])) {
